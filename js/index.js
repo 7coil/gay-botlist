8a075dcac3b69d8035c9b0a908c0350372f0fc60
lepon01 <katie@discordmail.com>
2018-08-07 20:26:45 +0100

    Use data from Discord Fork

--- js/index.js
+++ js/index.js
@@ -1,29 +1,26 @@
 (async () => {
   const botsDiv = document.getElementById('bots');
-  const bots = (await fetch('json/bots.json')
-    .then(data => data.json()))
-    .map(bot => ({
-      id: bot,
-      fetched: false
-    }));
+  const bots = await fetch('https://discordbots.co.uk/api/bots/all.json')
+    .then(data => data.json())
   
   const randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
   bots.forEach(async (bot) => {
-    const botData = await fetch(`json/${bot.id}.json`)
-      .then(data => data.json());
-
     const botDiv = document.createElement('div');
     const botHeader = document.createElement('h2');
-    botHeader.innerText = botData.name;
+    botHeader.innerText = bot.name;
     const botDescription = document.createElement('p');
-    botDescription.innerText = botData.description;
+    botDescription.innerText = bot.description;
     const botInvite = document.createElement('a');
     const botInviteLabel = document.createElement('p');
-    botInviteLabel.innerText = `Invite ${botData.name}`;
+    botInviteLabel.innerText = `Invite ${bot.name}`;
     botInvite.appendChild(botInviteLabel);
-    botInvite.setAttribute('href', botData.invite);
+    botInvite.setAttribute('href', bot.invite);
+    const botAvatar = document.createElement('img');
+    botAvatar.setAttribute('src', bot.avatar);
+    botAvatar.classList.add('avatar');
 
+    botDiv.appendChild(botAvatar);
     botDiv.appendChild(botHeader);
     botDiv.appendChild(botDescription);
     botDiv.appendChild(botInvite);
@@ -32,6 +29,8 @@
     botDiv.classList.add('bot-div');
     botDiv.style.animationDuration = `${(Math.random() * 20) + 0.5}s`;
     botDiv.style.color = `rgb(${randomInt(256)},${randomInt(256)},${randomInt(256)})`;
+    botDiv.style.width = `${randomInt(200) + 200}px`;
+    botDiv.style.height = `${randomInt(300) + 300}px`;
 
     botsDiv.appendChild(botDiv);
   });
